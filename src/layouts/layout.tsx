32548ff07a155eecfa5ed40ef14cffa8044f7a15
iHubG <ianmacalinao50@gmail.com>
2025-09-25 01:39:21 +0800

    fix reset to top

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import ScrollToTop from "@/components/ScrollToTop";
 
-const layout = () => {
+const Layout = () => {
   return (
     <div className="dark:bg-[#0f172a] pt-5 lg:pt-10 h-full">
+      <ScrollToTop /> 
       <Navbar />
 
       <main className="p-6">
@@ -16,4 +18,4 @@ const layout = () => {
   );
 };
 
-export default layout;
+export default Layout;
